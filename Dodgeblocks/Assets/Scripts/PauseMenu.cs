e880d9158939f0f0c4c6dac16c994faac7776818
charmseer <charmseer@gmail.com>
2021-06-04 23:28:49 +0530

    Make Pause Menu Buttons function and rotation smooth.

---
+++ Dodgeblocks/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class PauseMenu : MonoBehaviour
+{
+
+    public static bool GameIsPaused = false;
+    // Start is called before the first frame update
+    public GameObject PauseMenuUI;
+
+    void Update()
+    {
+
+    }
+
+    public void Resume()
+    {
+        PauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+    }
+
+    public void Pause()
+    {
+        PauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+    }
+
+    public void MainMenu()
+    {
+        PauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+        SceneManager.LoadScene("Menu");
+    }
+
+    public void EndGame()
+    {
+        Debug.Log("Player has chosen to quit the Game");
+        Application.Quit();
+    }
+}
