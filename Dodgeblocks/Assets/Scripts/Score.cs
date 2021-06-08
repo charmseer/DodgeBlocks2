116e450ae8b9f6da3a5cb645e8f35651d4b7beb7
charmseer <charmseer@gmail.com>
2021-06-08 23:45:06 +0530

    Failed attempt to create High Score with PlayerPref

--- Dodgeblocks/Assets/Scripts/Score.cs
+++ Dodgeblocks/Assets/Scripts/Score.cs
@@ -5,22 +5,28 @@ using UnityEngine.UI;
 
 public class Score : MonoBehaviour
 {
-    public Text scoreValue;
-    public Text scoreValue2;
+    public Text scoreValue; // For on screen active Score Object
+    public Text scoreValue2; // For score displayed when Game Over Panel's Score Object
 
-    public int counter;
 
-    private void Update()
+    public int counter; // Everytime a block collides with ScoreBase the counter goes up by one
+
+    private void Start()
     {
         
+    }
+    private void Update()
+    {
+
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        counter += 1; 
-        Debug.Log("Col lided with ScoreBase");
+        counter += 1;
+        Debug.Log("Collided with ScoreBase");
 
-        scoreValue.text = (counter / 4).ToString();
+        scoreValue.text = (counter / 4).ToString(); // 4 boxes collide with the Scorebase but score only needs to go up once 
         scoreValue2.text = (counter / 4).ToString();
+
     }
 }
