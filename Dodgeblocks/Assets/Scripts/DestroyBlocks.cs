c37b1870725f9217126b189cfface83514f12328
charmseer <charmseer@gmail.com>
2021-06-09 22:22:38 +0530

    Give uniform motion to block prefabs
    Set Mass and GravityScale of Block prefab to 0 and give it's Rigidbody a downwards velocity

--- Dodgeblocks/Assets/Scripts/DestroyBlocks.cs
+++ Dodgeblocks/Assets/Scripts/DestroyBlocks.cs
@@ -7,20 +7,27 @@ public class DestroyBlocks : MonoBehaviour
     //public float gravityVar = 5;
 
     private Rigidbody2D rb;
+    float downForce = -3.5f;
 
-    [SerializeField] public float downwardForce = 20f;
+    //[SerializeField] public float downwardForce = 20f;
+
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
-       // GetComponent<Rigidbody2D>().gravityScale += Time.timeSinceLevelLoad/gravityVar;
+        //GetComponent<Rigidbody2D>().gravityScale += Time.timeSinceLevelLoad/gravityVar;
+        rb.velocity = new Vector2(0f, downForce);
+
     }
 
+    void Start()
+    {   
+
+    }
     // Update is called once per frame
     void FixedUpdate()
     {
-        
-        rb.AddForce(new Vector2(0, downwardForce) * Time.deltaTime , ForceMode2D.Force);
+
     }
 
     void OnCollisionEnter2D()
