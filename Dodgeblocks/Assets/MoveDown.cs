411bb4461f56fe6c7c8575d037736f2fff665ab4
charmseer <charmseer@gmail.com>
2021-06-19 16:10:42 +0530

    Make Background descend

---
+++ Dodgeblocks/Assets/MoveDown.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveDown : MonoBehaviour
+{
+    float downForce = 3.5f;
+
+    //private float speed = 30;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector2.down * Time.deltaTime * downForce);
+        //transform.Translate(Vector3.left * Time.deltaTime * speed);
+    }
+}
